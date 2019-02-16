aa2c66d5dd0978a4fef8f55e9f7588fdc961a070
manojknit <erpatel@gmail.com>
2019-02-15 22:55:29 -0800

    script

--- instance.tf
+++ instance.tf
@@ -1,25 +1,31 @@
-resource "aws_key_pair" "mykey" {
-  key_name = "mykey"
-  public_key = "${file("${var.PATH_TO_PUBLIC_KEY}")}"
+resource "aws_key_pair" "mykey" { 
+  key_name = "mykey" #$ ssh-keygen -f mykey   #it will generate public and private key
+  public_key = "${file("${var.PATH_TO_PUBLIC_KEY}")}" #public key upload to aws
 }
 
 resource "aws_instance" "example" {
   ami = "${lookup(var.AMIS, var.AWS_REGION)}"
   instance_type = "t2.micro"
-  key_name = "${aws_key_pair.mykey.key_name}"
+  key_name = "${aws_key_pair.mykey.key_name}" #letting AWS know which public key need to be install
 
-  provisioner "file" {
+  provisioner "file" {  # upload file
     source = "script.sh"
     destination = "/tmp/script.sh"
+    /* login using user id and password. Best practice is to login using private and public key
+    connection{
+      user = "${var.instance_username}"
+      password = "${var.instance_password}"
+    }
+    */
   }
   provisioner "remote-exec" {
     inline = [
-      "chmod +x /tmp/script.sh",
+      "chmod +x /tmp/script.sh", # giving executable(+x) rights
       "sudo /tmp/script.sh"
     ]
   }
   connection {
     user = "${var.INSTANCE_USERNAME}"
-    private_key = "${file("${var.PATH_TO_PRIVATE_KEY}")}"
+    private_key = "${file("${var.PATH_TO_PRIVATE_KEY}")}" # private key to login
   }
 }
--- vars.tf
+++ vars.tf
@@ -1,14 +1,13 @@
 variable "AWS_ACCESS_KEY" {}
 variable "AWS_SECRET_KEY" {}
 variable "AWS_REGION" {
-  default = "eu-west-1"
+  default = "us-east-1"
 }
 variable "AMIS" {
   type = "map"
   default = {
-    us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-844e0bf7"
+    us-east-1 = "ami-0565af6e282977273"
+    us-west-1 = "ami-0ff7f191316dba328"
   }
 }
 
